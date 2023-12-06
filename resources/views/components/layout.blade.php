3fc2f11de1b95513c1d12dfc967e18ef832b77b5
Andrew Nelson <anelson2037@gmail.com>
2023-12-05 22:21:25 -0800

    Installing TALL

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        {{-- Local fonts found in app.css --}}
+        
+        <!-- Favicon -->
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💊</text></svg>">
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+    
+            <!-- Page Heading -->
+            @if (isset($header))
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endif
+    
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+    </body>
+    
+    
+</html>
---
+++ resources/views/rules/index.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <x-slot:header>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Rules') }}
+        </h2>
+    </x-slot:header>
+    Hi
+</x-layout>
