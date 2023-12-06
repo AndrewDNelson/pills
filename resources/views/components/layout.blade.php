d574812a8b4edf00e24f68c2c8fa2eef65dbf6e1
Andrew Nelson <anelson2037@gmail.com>
2023-12-05 22:25:10 -0800

    Fixing layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,8 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        
+        <title>{{ config('app.name') }}</title>
 
         <!-- Fonts -->
         {{-- Local fonts found in app.css --}}
@@ -14,7 +14,9 @@
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💊</text></svg>">
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+        @livewireStyles
+        @livewireScripts
     </head>
 
     <body class="font-sans antialiased">
